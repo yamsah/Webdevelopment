e49cac4574d84a72e0425541ebe069baf7c21606
yamsah <yamsahkisinja@gmail.com>
2023-04-23 13:29:29 -0700

    my commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    
+    document.querySelectorAll("img")[0].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("img")[1].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("img")[2].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("img")[3].setAttribute("src","https://media.giphy.com/media/jaqvaWqpKflmQ/giphy.gif");
+    document.querySelectorAll("p")[0].style.textTransform = "upperCase";
+    document.querySelectorAll("p")[1].style.textTransform = "upperCase";
+
+}
+
+myfunction();
